c60c39aa09361014ff4a9939bf0a0564696f76ee
jasonxue <jason@xuechunxi.com>
2025-03-02 10:47:33 +0800

    cmp

--- nvim/.config/nvim/lua/plugins/lsp/init.lua
+++ nvim/.config/nvim/lua/plugins/lsp/init.lua
@@ -91,6 +91,7 @@ later(function()
       "hrsh7th/cmp-cmdline",
       "abeldekat/cmp-mini-snippets",
       "nvim-lua/plenary.nvim",
+      "hrsh7th/cmp-nvim-lua",
       "petertriho/cmp-git",
     },
   })
@@ -102,13 +103,8 @@ later(function()
     snippet = {
       -- REQUIRED - you must specify a snippet engine
       expand = function(args)
-        -- vim.fn["vsnip#anonymous"](args.body) -- For `vsnip` users.
-        -- require('luasnip').lsp_expand(args.body) -- For `luasnip` users.
-        -- require('snippy').expand_snippet(args.body) -- For `snippy` users.
-        -- vim.fn["UltiSnips#Anon"](args.body) -- For `ultisnips` users.
-        -- vim.snippet.expand(args.body) -- For native neovim snippets (Neovim v0.10+)
-
         -- For `mini.snippets` users:
+        MiniSnippets = require("mini.snippets")
         local insert = MiniSnippets.config.expand.insert or MiniSnippets.default_insert
         insert({ body = args.body }) -- Insert at cursor
         cmp.resubscribe({ "TextChangedI", "TextChangedP" })
@@ -125,15 +121,46 @@ later(function()
       ["<C-Space>"] = cmp.mapping.complete(),
       ["<C-e>"] = cmp.mapping.abort(),
       ["<CR>"] = cmp.mapping.confirm({ select = true }), -- Accept currently selected item. Set `select` to `false` to only confirm explicitly selected items.
+
+      ["<Tab>"] = function(fallback)
+        if cmp.visible() then
+          cmp.select_next_item()
+        else
+          fallback()
+        end
+      end,
+
+      ["<S-Tab>"] = function(fallback)
+        if cmp.visible() then
+          cmp.select_prev_item()
+        else
+          fallback()
+        end
+      end,
+
+      ["<c-j>"] = function(fallback)
+        if cmp.visible() then
+          cmp.select_next_item()
+        else
+          fallback()
+        end
+      end,
+
+      ["<c-k>"] = function(fallback)
+        if cmp.visible() then
+          cmp.select_prev_item()
+        else
+          fallback()
+        end
+      end,
     }),
+
     sources = cmp.config.sources({
       { name = "nvim_lsp" },
-      { name = "MiniSnippets" }, -- For vsnip users.
-      -- { name = 'luasnip' }, -- For luasnip users.
-      -- { name = 'ultisnips' }, -- For ultisnips users.
-      -- { name = 'snippy' }, -- For snippy users.
-    }, {
-      { name = "buffer" },
+      { name = "mini_snippets" },
+      { name = "path" },
+      { name = "buffer", keyword_length = 5 },
+      { name = "nvim_lua" },
     }),
   })
 
@@ -143,7 +170,6 @@ later(function()
   cmp.setup.filetype("gitcommit", {
     sources = cmp.config.sources({
       { name = "git" },
-    }, {
       { name = "buffer" },
     }),
   })
@@ -162,13 +188,13 @@ later(function()
     mapping = cmp.mapping.preset.cmdline(),
     sources = cmp.config.sources({
       { name = "path" },
-    }, {
       { name = "cmdline" },
     }),
     matching = { disallow_symbol_nonprefix_matching = false },
   })
 end)
 -- }}}
+
 -- -- mini.completion {{{
 -- later(function()
 --   require("mini.completion").setup()

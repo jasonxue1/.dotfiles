3b3c81ab4fdef78cb275f0aa463d4ec5b4be09eb
jasonxue <jason@xuechunxi.com>
2025-04-19 11:26:45 +0800

    1

--- fish/.config/fish/conf.d/function.fish
+++ fish/.config/fish/conf.d/function.fish
@@ -1,3 +1,17 @@
 function mkcd
   mkdir -p $argv && cd $argv
 end
+
+# py venv
+function auto_activate_venv --on-variable PWD
+    # 如果当前目录下有 .venv（或 venv）文件夹，就激活它
+    if test -d .venv
+        source .venv/bin/activate.fish
+    else if test -d venv
+        source venv/bin/activate.fish
+
+    # 否则如果已经有激活的 VIRTUAL_ENV，就停用它
+    else if set -q VIRTUAL_ENV
+        deactivate
+    end
+end
